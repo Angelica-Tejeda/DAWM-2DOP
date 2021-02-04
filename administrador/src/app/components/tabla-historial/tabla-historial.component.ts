8a144157df7d99c39d54d74df16e7ebf3ec789ec
Luis Sanchez <lcsanche@espol.edu.ec>
2021-02-04 00:03:57 -0500

    Filtrado de historial a traves de ambas bases de datos

--- administrador/src/app/components/tabla-historial/tabla-historial.component.ts
+++ administrador/src/app/components/tabla-historial/tabla-historial.component.ts
@@ -11,9 +11,28 @@ export class TablaHistorialComponent implements OnInit {
   datos: any = [];
 
   constructor(private historialServices: HistorialService) {
+    let script = document.createElement("script");
+    script.src = "assets/javascript/historial.js";
+    
+    let body = document.getElementsByTagName("body")[0]
+    body.appendChild(script);
   }
 
   ngOnInit() {
+    let filtro = document.getElementById("filtro")
+      
+    fetch("http://localhost:3000/relacional/menu")
+      .then((resultado) =>{
+          return resultado.json();
+      })
+      .then((json) =>{
+          for(let elemento of json){
+              let opcion = document.createElement("option")
+              opcion.textContent = elemento.nombre;
+              filtro.appendChild(opcion)
+          }
+      })
+
     this.historialServices.getHistorial().subscribe(
       res => {
         this.datos = res;
